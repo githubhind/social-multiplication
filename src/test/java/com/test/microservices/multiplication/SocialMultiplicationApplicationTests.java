dfbcbfe03dfa1013092d06eeef949b044869327c
Github Hind <githubhind@gmail.com>
2021-01-27 21:56:53 +0100

    Add class with main method

---
+++ src/test/java/com/test/microservices/multiplication/SocialMultiplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.microservices.multiplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMultiplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
