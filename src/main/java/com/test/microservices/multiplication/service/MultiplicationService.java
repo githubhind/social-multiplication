09e0a8fc59ac8a4aecd62e402a39e2fd25489c0e
Github Hind <githubhind@gmail.com>
2021-02-01 22:38:46 +0100

    Add multiplication service

---
+++ src/main/java/com/test/microservices/multiplication/service/MultiplicationService.java
@@ -0,0 +1,8 @@
+package com.test.microservices.multiplication.service;
+
+import com.test.microservices.multiplication.domain.Multiplication;
+
+public interface MultiplicationService {
+
+    Multiplication createRandomMultiplication();
+}
---
+++ src/main/java/com/test/microservices/multiplication/service/RandomGeneratorService.java
@@ -0,0 +1,7 @@
+package com.test.microservices.multiplication.service;
+
+public interface RandomGeneratorService {
+
+    int generateRandomFactor();
+
+}
