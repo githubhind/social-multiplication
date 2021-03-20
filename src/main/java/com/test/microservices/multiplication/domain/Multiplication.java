0c2f5bbbe6f4a7b9ecae30e0c7ff155a594468d8
Github Hind <githubhind@gmail.com>
2021-03-20 16:58:58 +0100

    Add Entity to domain models

--- src/main/java/com/test/microservices/multiplication/domain/Multiplication.java
+++ src/main/java/com/test/microservices/multiplication/domain/Multiplication.java
@@ -5,12 +5,21 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 @RequiredArgsConstructor
 @Getter
 @ToString
 @EqualsAndHashCode
+@Entity
 public final class Multiplication {
 
+    @Id
+    @GeneratedValue
+    private Long id;
     private final int factorA;
     private final int factorB;
 
--- src/main/java/com/test/microservices/multiplication/domain/User.java
+++ src/main/java/com/test/microservices/multiplication/domain/User.java
@@ -2,12 +2,21 @@ package com.test.microservices.multiplication.domain;
 
 import lombok.*;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 @RequiredArgsConstructor
 @Getter
 @ToString
 @EqualsAndHashCode
+@Entity
 public final class User {
 
+    @Id
+    @GeneratedValue
+    private Long id;
     private final String alias;
 
     public User() {
