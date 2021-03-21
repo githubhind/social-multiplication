c5050e0feb4d315d3f85567d3c8751c52fb205ed
Github Hind <githubhind@gmail.com>
2021-03-21 22:41:39 +0100

    Persist user attempt

--- src/main/java/com/test/microservices/multiplication/service/MultiplicationServiceImpl.java
+++ src/main/java/com/test/microservices/multiplication/service/MultiplicationServiceImpl.java
@@ -2,17 +2,27 @@ package com.test.microservices.multiplication.service;
 
 import com.test.microservices.multiplication.domain.Multiplication;
 import com.test.microservices.multiplication.domain.MultiplicationResultAttempt;
+import com.test.microservices.multiplication.domain.User;
+import com.test.microservices.multiplication.repository.MultiplicationResultAttemptRepository;
+import com.test.microservices.multiplication.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Service
 public class MultiplicationServiceImpl implements MultiplicationService {
 
     private RandomGeneratorService randomGeneratorService;
+    private UserRepository userRepository;
+    private MultiplicationResultAttemptRepository multiplicationResultAttemptRepository;
 
     @Autowired
-    public MultiplicationServiceImpl(RandomGeneratorService randomGeneratorService) {
+    public MultiplicationServiceImpl(RandomGeneratorService randomGeneratorService, UserRepository userRepository, MultiplicationResultAttemptRepository multiplicationResultAttemptRepository) {
         this.randomGeneratorService = randomGeneratorService;
+        this.userRepository = userRepository;
+        this.multiplicationResultAttemptRepository = multiplicationResultAttemptRepository;
     }
 
     @Override
@@ -22,11 +32,21 @@ public class MultiplicationServiceImpl implements MultiplicationService {
         return new Multiplication(factorA, factorB);
     }
 
+    @Transactional
     @Override
     public boolean checkAttempt(final MultiplicationResultAttempt multiplicationResultAttempt) {
-        return multiplicationResultAttempt.getResultAttempt() ==
+        boolean verification = multiplicationResultAttempt.getResultAttempt() ==
                 multiplicationResultAttempt.getMultiplication().getFactorA() *
                         multiplicationResultAttempt.getMultiplication().getFactorB();
+        Optional<User> user = userRepository.findByAlias(multiplicationResultAttempt.getUser().getAlias());
+        MultiplicationResultAttempt multiplicationResultAttemptVerified = new MultiplicationResultAttempt(
+                user.orElse(multiplicationResultAttempt.getUser()),
+                multiplicationResultAttempt.getMultiplication(),
+                multiplicationResultAttempt.getResultAttempt(),
+                verification
+        );
+        multiplicationResultAttemptRepository.save(multiplicationResultAttemptVerified);
+        return verification;
     }
 
 }
--- src/test/java/com/test/microservices/multiplication/service/MultiplicationServiceImplTest.java
+++ src/test/java/com/test/microservices/multiplication/service/MultiplicationServiceImplTest.java
@@ -3,25 +3,37 @@ package com.test.microservices.multiplication.service;
 import com.test.microservices.multiplication.domain.Multiplication;
 import com.test.microservices.multiplication.domain.MultiplicationResultAttempt;
 import com.test.microservices.multiplication.domain.User;
+import com.test.microservices.multiplication.repository.MultiplicationResultAttemptRepository;
+import com.test.microservices.multiplication.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 
 public class MultiplicationServiceImplTest {
 
     @Mock
     private RandomGeneratorService randomGeneratorService;
 
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private MultiplicationResultAttemptRepository multiplicationResultAttemptRepository;
+
     private MultiplicationServiceImpl multiplicationServiceImpl;
 
     @BeforeEach
     public void setUp() {
         MockitoAnnotations.initMocks(this);
-        multiplicationServiceImpl = new MultiplicationServiceImpl(randomGeneratorService);
+        multiplicationServiceImpl = new MultiplicationServiceImpl(randomGeneratorService, userRepository, multiplicationResultAttemptRepository);
     }
 
     @Test
@@ -43,12 +55,15 @@ public class MultiplicationServiceImplTest {
         User user = new User("john_doe");
         Multiplication multiplication = new Multiplication(50, 60);
         MultiplicationResultAttempt multiplicationResultAttempt = new MultiplicationResultAttempt(user, multiplication, 3000, false);
+        MultiplicationResultAttempt multiplicationResultAttemptVerified = new MultiplicationResultAttempt(user, multiplication, 3000, true);
+        given(userRepository.findByAlias("john_doe")).willReturn(Optional.empty());
 
         //when
         boolean result = multiplicationServiceImpl.checkAttempt(multiplicationResultAttempt);
 
         //then
         assertThat(result).isTrue();
+        verify(multiplicationResultAttemptRepository).save(multiplicationResultAttemptVerified);
     }
 
     @Test
@@ -57,11 +72,13 @@ public class MultiplicationServiceImplTest {
         User user = new User("john_doe");
         Multiplication multiplication = new Multiplication(50, 60);
         MultiplicationResultAttempt multiplicationResultAttempt = new MultiplicationResultAttempt(user, multiplication, 3010, false);
+        BDDMockito.given(userRepository.findByAlias("john_doe")).willReturn(Optional.empty());
 
         //when
         boolean result = multiplicationServiceImpl.checkAttempt(multiplicationResultAttempt);
 
         //then
         assertThat(result).isFalse();
+        BDDMockito.verify(multiplicationResultAttemptRepository).save(multiplicationResultAttempt);
     }
 }
